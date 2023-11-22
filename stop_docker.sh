decab73e626b6de978e3c590a6c8bb10294af810
analog <kimchesed.paller@analog.com>
2023-11-22 16:49:41 +0800

    add script for stoping container
    Signed-off-by: analog <kimchesed.paller@analog.com>

---
+++ stop_docker.sh
@@ -0,0 +1,14 @@
+set -e
+CONTAINER=$(docker ps -a --filter ancestor="constellation" --format "{{.ID}}")
+IMAGE=$(docker ps -a --filter ancestor="constellation" --format "{{.Image}}")
+if [ "$CONTAINER" ]
+then
+	echo "FOUND"
+	docker stop $CONTAINER
+	docker rm $CONTAINER
+	docker image rm $IMAGE
+fi
+
+#docker build -t constellation .
+# docker run  -d --restart unless-stopped -p 5000:5000 constellation
+#docker run -d --restart unless-stopped -p 5000:5000 constellation --env GH_TOKEN=$GH_TOKEN
