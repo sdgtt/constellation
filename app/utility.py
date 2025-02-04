3f2e98a484a884c8385ae7ed371b7f5967cb2f9b
Say <clarissa.septimo@analog.com>
2025-02-03 20:05:16 -0500

    fix in trigger url
    Signed-off-by: Say <clarissa.septimo@analog.com>

--- app/utility.py
+++ app/utility.py
@@ -45,7 +45,7 @@ def url_gen(
         if trigger_dict[0] == "auto":
             trigger_project_name = trigger_dict[1].split("/")[0]
             trigger_build_number = trigger_dict[2]
-            url_trigger = "http://{}/job/{}/{}/".format(
+            url_trigger = "http://{}/view/all/job/Triggers/job/{}/{}/".format(
                 jenkins_server, trigger_project_name, trigger_build_number
             )
     else:
