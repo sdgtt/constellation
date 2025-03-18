13641605ed88c4e7ad2b5035b1746454b9e09a42
Say <clarissa.septimo@analog.com>
2025-03-18 03:27:24 -0400

    remove port and cert in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-flask run --host=0.0.0.0 --port=5000 --cert=./ssl/cert.crt --key=./ssl/cert.key
-# flask run --host=0.0.0.0
+# flask run --host=0.0.0.0 --port=5000 --cert=./ssl/cert.crt --key=./ssl/cert.key
+flask run --host=0.0.0.0
 # flask run --host=0.0.0.0 --port=5000 --cert=./ssl/cos_analog_com.crt --key=./ssl/cos_analog_com.key
